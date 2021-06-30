e1a847b49e047e9c9c8f410f15533cf43afc2301
reda aj <ajendouz.medreda@gmail.com>
2021-06-30 16:35:56 +0100

    save

--- gestio-stock/api/getPhones.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbh = new PDO("mysql:host=localhost;dbname=Phone","root","Tanger123");
-$sql = " SELECT * FROM Phones ";
-$phonesQuery = $dbh->query($sql);
-$getphones = $phonesQuery->fetchAll(PDO::FETCH_ASSOC);
-print_r(json_encode($getphones));
-?>
